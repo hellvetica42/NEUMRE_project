ffcbbc893884d0a57a96de6b15aa85d37e57b922
hellvetica42 <petar.slavicek@gmail.com>
2022-01-13 03:44:15 +0100

    Working, not done

--- Character.py
+++ Character.py
@@ -1,65 +1,176 @@
 from os import listdir
 from os.path import isfile, join
+import json
+from typing import OrderedDict
 
 import cv2
 import numpy as np
 
-from utils import overlay_transparent, rotate_image, drawDebugPoints, getAngle
+from utils import drawDebugPoints, getRotatedImageWithAnchor, overlay_transparent, getAngle, getRotatedImageWithAnchor2
 from constants import INDICIES
 
 class BodyPart:
-    def __init__(self, image, originalImage, xpos, ypos) -> None:
+    def __init__(self, image) -> None:
         self.image = image
-        self.originalImage = originalImage
-        self.xpos, self.ypos = xpos, ypos
-
-    def get(self):
-        return self.image, self.originalImage, self.xpos, self.ypos
 
 class Character:
     def __init__(self, filepath) -> None:
         self.filepath = filepath 
         self.bodyparts = self.readBodyparts(self.filepath)
+        self.angle=0
 
     def readBodyparts(self, filepath):
-        files = [f for f in listdir(filepath) if isfile(join(filepath, f))]
+        files = [f for f in listdir(filepath) if isfile(join(filepath, f)) and ".png" in f]
         bodyparts = {}
         for f in files:
             key = "".join(f.split('-')[1:])[:-4]
 
             image = cv2.imread(join(filepath,f), cv2.IMREAD_UNCHANGED)
-            longer = max(image.shape[0], image.shape[1])
-            tmpImg = np.zeros((longer*2, longer*2, image.shape[2]))
+            bodyparts[key] = BodyPart(image)
 
-            xpos = (tmpImg.shape[1] // 2) - image.shape[1] // 2
-            ypos = (tmpImg.shape[0] // 2) - image.shape[0] // 2
+        with open(join(filepath, "rigging.json"), 'r') as f:
+            self.rigging = json.load(f)
 
-            tmpImg[ypos:ypos+image.shape[0], xpos:xpos+image.shape[1]] = image
-
-            bodyparts[key] = BodyPart(tmpImg, image, xpos, ypos)
-
-        print(bodyparts)
+        print(self.rigging)
         return bodyparts
 
 
     def drawCharacter(self, keypoints, frame):
+        self.angle += 1
+        #TORSO IS ANCHOR
+        p1 = keypoints[INDICIES["leftShoulder"]]
+        #torsopos = p1
+        torsopos = np.array([200, 200])
+
+        #LEFT ARM UPPER
+        bp = self.bodyparts['armupper'].image
+
+        anchor1 = np.array((20, bp.shape[1]//2))
+        anchor2 = np.array((bp.shape[0]-20, bp.shape[1]//2))
+
+        offset = np.array(self.rigging['left-arm-upper'])
+
+        p2 = keypoints[INDICIES["leftElbow"]]
+        p1 = keypoints[INDICIES["leftShoulder"]]
+        diff = p1 - p2
+        angle, anglerad = getAngle(diff)
+        #angle, anglerad = self.angle, np.deg2rad(self.angle)
+
+        leftArmUpper, anchor1, anchor2 = getRotatedImageWithAnchor2(bp, anchor1, anchor2, angle, anglerad)
+
+        leftShoulder = [int(torsopos[0] + offset[0] - anchor1[0]), 
+                        int(torsopos[1] + offset[1] - anchor1[1])]
+
 
         #LEFT ARM LOWER
-        bp, _, xpos, ypos = self.bodyparts['armlower'].get()
+        bp = self.bodyparts['armlower'].image
 
-        bp = drawDebugPoints(bp)
+        anchor = np.array((20, bp.shape[1]//2))
 
-        p1 = keypoints[INDICIES["leftElbow"]]
-        p2 = keypoints[INDICIES["leftWrist"]]
+        offset = np.array(self.rigging['left-arm-lower'])
+
+        p2 = keypoints[INDICIES["leftElbow"]]
+        p1 = keypoints[INDICIES["leftWrist"]]
+        diff = p1 - p2
+        angle, anglerad = getAngle(diff)
+
+        bp, anchor = getRotatedImageWithAnchor(bp, anchor, angle, anglerad)
+
+        leftElbow = [int(leftShoulder[0] + anchor2[0] - anchor[0]), 
+                      int(leftShoulder[1] + anchor2[1] - anchor[1])]
+
+        frame = overlay_transparent(frame, bp, leftElbow[1], leftElbow[0])
+        frame = overlay_transparent(frame, leftArmUpper, leftShoulder[1], leftShoulder[0])
+
+        #LOWER TORSO
+        bp = self.bodyparts['torsolower'].image
+        offset = np.array(self.rigging['torso-lower'])
+        frame = overlay_transparent(frame, bp, int(torsopos[1] + offset[1]), int(torsopos[0] + offset[0]))
+
+        #TORSO
+        bp = self.bodyparts['torsoupper'].image
+        frame = overlay_transparent(frame, bp, int(torsopos[1]), int(torsopos[0]))
+
+        #RIGHT ARM UPPER
+        bp = self.bodyparts['armupper'].image
+
+        anchor1 = np.array((20, bp.shape[1]//2))
+        anchor2 = np.array((bp.shape[0]-20, bp.shape[1]//2))
+
+        offset = np.array(self.rigging['right-arm-upper'])
+
+        p2 = keypoints[INDICIES["rightElbow"]]
+        p1 = keypoints[INDICIES["rightShoulder"]]
+        diff = p1 - p2
+        angle, anglerad = getAngle(diff)
+        #angle, anglerad = self.angle, np.deg2rad(self.angle)
+
+        rightArmUpper, anchor1, anchor2 = getRotatedImageWithAnchor2(bp, anchor1, anchor2, angle, anglerad)
+
+        rightShoulder = [int(torsopos[0] + offset[0] - anchor1[0]), 
+                        int(torsopos[1] + offset[1] - anchor1[1])]
+
+        #RIGHT ARM LOWER
+        bp = self.bodyparts['armlower'].image
+
+        anchor = np.array((20, bp.shape[1]//2))
+
+        offset = np.array(self.rigging['right-arm-lower'])
+
+        p2 = keypoints[INDICIES["rightElbow"]]
+        p1 = keypoints[INDICIES["rightWrist"]]
+        diff = p1 - p2
+        angle, anglerad = getAngle(diff)
+
+        bp, anchor = getRotatedImageWithAnchor(bp, anchor, angle, anglerad)
+
+        rightElbow = [int(rightShoulder[0] + anchor2[0] - anchor[0]), 
+                      int(rightShoulder[1] + anchor2[1] - anchor[1])]
+
+        frame = overlay_transparent(frame, bp, rightElbow[1], rightElbow[0])
+
+        #Draw right upper arm over lower
+        frame = overlay_transparent(frame, rightArmUpper, rightShoulder[1], rightShoulder[0])
+
+
+        #RIGHT LEG UPPER
+        bp = cv2.flip(self.bodyparts['legupper'].image, 1)
+
+        anchor1 = np.array((20, bp.shape[1]//2))
+        anchor2 = np.array((bp.shape[0]-20, bp.shape[1]//2))
+
+        offset = np.array(self.rigging['right-leg-upper'])
+
+        p2 = keypoints[INDICIES["rightHip"]]
+        p1 = keypoints[INDICIES["rightKnee"]]
         diff = p2 - p1
-        size = np.linalg.norm(diff)
-        angle = getAngle(diff)
+        angle, anglerad = getAngle(diff)
+        #angle, anglerad = self.angle, np.deg2rad(self.angle)
+
+        rightLegUpper, anchor1, anchor2 = getRotatedImageWithAnchor2(bp, anchor1, anchor2, angle, anglerad)
+
+        rightHip = [int(torsopos[0] + offset[0] - anchor1[0]), 
+                        int(torsopos[1] + offset[1] - anchor1[1])]
 
 
+        #RIGHT LEG LOWER
+        bp = self.bodyparts['leglower'].image
 
-        
+        anchor = np.array((20, bp.shape[1]//2))
 
-        bp = rotate_image(bp, angle)
-        frame = overlay_transparent(frame, bp, int(p1[1])-ypos, int(p1[0])-xpos)
+        offset = np.array(self.rigging['right-leg-lower'])
 
-        #bp = cv2.resize(bp, (bp.shape[0]+self.angle, bp.shape[1]+self.angle))
+        p2 = keypoints[INDICIES["rightAnkle"]]
+        p1 = keypoints[INDICIES["rightKnee"]]
+        diff = p1 - p2
+        angle, anglerad = getAngle(diff)
+
+        bp, anchor = getRotatedImageWithAnchor(bp, anchor, angle, anglerad)
+
+        rightKnee = [int(rightLegUpper[0] + anchor2[0] - anchor[0]), 
+                      int(rightLegUpper[1] + anchor2[1] - anchor[1])]
+
+        frame = overlay_transparent(frame, bp, rightKnee[1], rightKnee[0])
+
+        #Draw right upper leg over lower
+        frame = overlay_transparent(frame, rightLegUpper, rightHip [1], rightHip[0])
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cv2 
 import numpy as np
+from scipy import ndimage
 
 PART_NAMES = [
     "nose", "leftEye", "rightEye", "leftEar", "rightEar", "leftShoulder",
@@ -43,18 +44,73 @@ def drawSkel(keypoints, image):
 
 def getAngle(vec):
     angle = np.arctan2(vec[1], vec[0])
-    return np.rad2deg(angle)
+    return np.rad2deg(angle), angle
 
 def drawDebugPoints(image):
-    image = cv2.circle(image, (0,0), 3, (0,0,255,255), -1)
-    image = cv2.circle(image, (image.shape[0],image.shape[1]), 3, (0,0,255,255), 3)
+    image = cv2.circle(image, (0,0), 10, (0,0,255,255), -1)
+    image = cv2.circle(image, (image.shape[1],image.shape[0]), 10, (0,0,255,255), 3)
     return image
 
-def rotate_image(image, angle):
-    image_center = tuple(np.array(image.shape[1::-1]) / 2)
-    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-    return result
+def rotateVector(vector, theta):
+    rot = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+    return np.dot(rot, vector)
+
+def rotate_image(mat, angle):
+    """
+    Rotates an image (angle in degrees) and expands image to avoid cropping
+    """
+
+    height, width = mat.shape[:2] # image shape has 3 dimensions
+    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+
+    # rotation calculates the cos and sin, taking absolutes of those.
+    abs_cos = abs(rotation_mat[0,0]) 
+    abs_sin = abs(rotation_mat[0,1])
+
+    # find the new width and height bounds
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+    # rotate image with the new bounds and translated rotation matrix
+    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
+
+def getRotatedImageWithAnchor(image, anchor, angle, anglerad):
+        height, width = image.shape[:2]
+        image_center = np.array((height/2, width/2))
+
+        anchorVec = image_center - anchor
+
+        image = rotate_image(image, angle)
+
+        height, width = image.shape[:2]
+        image_center = np.array((height/2, width/2))
+
+        anchor = image_center - rotateVector(anchorVec, anglerad)
+        return image, anchor
+
+def getRotatedImageWithAnchor2(image, anchor1, anchor2, angle, anglerad):
+        height, width = image.shape[:2]
+        image_center = np.array((height/2, width/2))
+
+        anchorVec1 = image_center - anchor1
+        anchorVec2 = image_center - anchor2
+
+        image = rotate_image(image, angle)
+
+        height, width = image.shape[:2]
+        image_center = np.array((height/2, width/2))
+
+        anchor1 = image_center + rotateVector(anchorVec1, anglerad)
+        anchor2 = image_center + rotateVector(anchorVec2, anglerad)
+
+        return image, anchor1, anchor2
 
 def overlay_transparent(background, overlay, x, y):
 
